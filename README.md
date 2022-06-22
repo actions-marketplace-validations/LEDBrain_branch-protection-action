c91b28d45531a3f25e993a47b0441123b5c40a69
kdev <konstantinw@ledbrain.de>
2022-06-22 20:27:47 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Branch Protection Action
+
+This template includes tests, linting, a validation workflow, publishing, and versioning guidance.
+
+## Usage
+
+```yaml
+uses: LEDBrain/branch-protection-action@v1.0.0
+with:
+    # Optional: branch to lift the restriction on; defaults to the current branch
+    branch: main
+    # Required
+    token: ${{ secrets.GH_PAT }}
+```
+
+## Code in Main
+
+Install the dependencies
+
+```bash
+npm install
+```
+
+Run prepare script before commit
+
+```bash
+npm run prepare
+```
+
+## Package for distribution
+
+GitHub Actions will run the entry point from the action.yml. Packaging assembles the code into one file that can be checked in to Git, enabling fast and reliable execution and preventing the need to check in node_modules.
+
+Actions are run from GitHub repos. Packaging the action will create a packaged action in the dist folder.
+
+Run prepare
+
+```bash
+npm run prepare
+```
+
+Since the packaged index.js is run from the dist folder.
+
+```bash
+git add dist
+```
+
+## Create a release branch
+
+Users shouldn't consume the action from master since that would be latest code and actions can break compatibility between major versions.
+
+Checkin to the v1 release branch
+
+```bash
+git checkout -b v1
+git commit -a -m "release: <commit>"
+```
+
+```bash
+git push origin v1
+```
+
+Note: We recommend using the `--license` option for ncc, which will create a license file for all of the production node modules used in your project.
+
+Your action is now published! :rocket:
+
+See the [versioning documentation](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md)
---
+++ index.js
@@ -0,0 +1,179 @@
+const core = require('@actions/core');
+const { getOctokit, context } = require('@actions/github');
+
+/**
+ * 3 Steps:
+ * 1. Get initial branch protection rule for current branch (main mostly)
+ * 2. Disable all branch protection rules for current branch
+ * 3. After actions completed, restore branch protection rule to initial state
+ */
+
+// most @actions toolkit packages have async methods
+async function run() {
+    const octokit = getOctokit(
+        core.getInput('token', {
+            required: true,
+        })
+    );
+
+    try {
+        if (!core.getState('STATE_branch-protection-has-run')) {
+            const branch = core.getInput('branch', { required: true }),
+                owner = context.repo.owner,
+                repo = context.repo.repo;
+            core.debug(branch, owner, repo);
+
+            const { data: state } =
+                await octokit.rest.repos.getBranchProtection({
+                    branch,
+                    owner,
+                    repo,
+                });
+            core.debug(state);
+            core.saveState('STATE_initial-branch-protection', state);
+            await octokit.rest.repos.deleteBranchProtection({
+                branch,
+                owner,
+                repo,
+            });
+            // await octokit.rest.repos.updateBranchProtection({
+            //     branch,
+            //     owner,
+            //     repo,
+            //     restrictions: null,
+            //     required_pull_request_reviews: null,
+            //     required_signatures: {},
+            //     enforce_admins: false,
+            //     required_linear_history: false,
+            //     allow_force_pushes: true,
+            //     allow_deletions: true,
+            //     block_creations: false,
+            //     required_conversation_resolution: false,
+            //     required_status_checks: null,
+            // });
+            core.saveState('STATE_branch-protection-has-run', true);
+        } else {
+            const branch = core.getInput('branch', { required: true }),
+                owner = context.repo.owner,
+                repo = context.repo.repo;
+            core.debug(branch, owner, repo);
+
+            const initState = JSON.parse(
+                core.getState('STATE_initial-branch-protection')
+            );
+            core.debug(initState);
+            const updatedState =
+                await octokit.rest.repos.updateBranchProtection({
+                    branch,
+                    owner,
+                    repo,
+
+                    restrictions: initState.restrictions
+                        ? {
+                              users:
+                                  initState.restrictions?.users?.map(
+                                      (u) => u?.login
+                                  ) || [],
+                              teams:
+                                  initState.restrictions?.teams?.map(
+                                      (t) => t?.slug
+                                  ) || [],
+                              apps:
+                                  initState.restrictions?.apps?.map(
+                                      (a) => a?.slug
+                                  ) || [],
+                          }
+                        : null,
+                    required_pull_request_reviews: {
+                        dismissal_restrictions: initState
+                            .required_pull_request_reviews
+                            ?.dismissal_restrictions
+                            ? {
+                                  users:
+                                      initState.required_pull_request_reviews?.dismissal_restrictions?.users?.map(
+                                          (u) => u?.login
+                                      ) || [],
+                                  teams:
+                                      initState.required_pull_request_reviews?.dismissal_restrictions?.teams?.map(
+                                          (t) => t?.slug
+                                      ) || [],
+                              }
+                            : null,
+                        dismiss_stale_reviews:
+                            initState.required_pull_request_reviews
+                                ?.dismiss_stale_reviews || false,
+                        require_code_owner_reviews:
+                            initState.required_pull_request_reviews
+                                ?.require_code_owner_reviews || false,
+                        required_approving_review_count:
+                            initState.required_pull_request_reviews
+                                ?.required_approving_review_count || 0,
+                        bypass_pull_request_allowances: initState
+                            .required_pull_request_reviews
+                            ?.bypass_pull_request_allowances
+                            ? {
+                                  users:
+                                      initState.required_pull_request_reviews?.bypass_pull_request_allowances?.users?.map(
+                                          (u) => u?.login
+                                      ) || [],
+                                  teams:
+                                      initState.required_pull_request_reviews?.bypass_pull_request_allowances?.teams?.map(
+                                          (t) => t?.slug
+                                      ) || [],
+                                  apps:
+                                      initState.required_pull_request_reviews?.bypass_pull_request_allowances?.apps?.map(
+                                          (a) => a?.slug
+                                      ) || [],
+                              }
+                            : null,
+                    },
+
+                    required_signatures:
+                        initState.required_signatures?.enabled || null,
+
+                    enforce_admins: initState.enforce_admins?.enabled || null,
+
+                    required_linear_history:
+                        initState.required_linear_history?.enabled || false,
+
+                    allow_force_pushes:
+                        initState.allow_force_pushes?.enabled || false,
+
+                    allow_deletions:
+                        initState.allow_deletions?.enabled || false,
+
+                    block_creations:
+                        initState.block_creations?.enabled || false,
+
+                    required_conversation_resolution:
+                        initState.required_conversation_resolution?.enabled ||
+                        false,
+
+                    required_status_checks: {
+                        strict:
+                            initState.required_status_checks?.strict || false,
+                        // contexts:
+                        //     initState.required_status_checks?.contexts || [],
+                        checks:
+                            initState.required_status_checks?.checks.map(
+                                (c) => ({
+                                    context: c?.context,
+                                    app_id: c?.app_id,
+                                })
+                            ) || undefined,
+                    },
+                });
+            core.debug(updatedState);
+        }
+    } catch (error) {
+        core.debug(error);
+        if (error.code === 404 && error.message === 'Branch not protected') {
+            core.notice('Branch not protected');
+            core.saveState('STATE_branch-protection-has-run', null);
+        } else {
+            core.setFailed(error.message);
+        }
+    }
+}
+
+run();
